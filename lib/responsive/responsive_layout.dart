58f3e1f33ed01d381d2d3017fbe211c02720873c
OrewaKimi <kimi93.com@gmail.com>
2025-04-19 12:11:47 +0700

    desktop display

--- lib/responsive/responsive_layout.dart
+++ lib/responsive/responsive_layout.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ResponsiveLayout extends StatelessWidget {
-  const ResponsiveLayout({super.key});
+  final Widget mobileBody;
+  final Widget desktopBody;
+
+  ResponsiveLayout({required this.mobileBody, required this.desktopBody});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-      
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileBody;
+        } else {
+          return desktopBody;
+        }
+      },
     );
   }
 }
